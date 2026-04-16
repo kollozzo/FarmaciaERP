6a6f6fb01e89e0d9c1905e26abe5b231c90a3ec2
jancito13 <janpachriv.13@gmail.com>
2026-04-16 16:15:29 -0500

    Update Producto.cs

--- FarmaciaERP.Domain/Entities/Producto.cs
+++ FarmaciaERP.Domain/Entities/Producto.cs
@@ -4,6 +4,14 @@ namespace FarmaciaERP.Domain.Entities
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string RegistroSanitario { get; set; }
         public decimal Precio { get; set; }
+        public int CategoriaId { get; set; }
+        
+        // Propiedades de navegación
+        public virtual Categoria Categoria { get; set; }
+        public virtual ICollection<Lote> Lotes { get; set; } = new List<Lote>();
+        public virtual ICollection<Stock> Stocks { get; set; } = new List<Stock>();
     }
 }
